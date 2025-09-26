546d23f2a100fcf41b8758c67d7f9cca0a596c98
DKM7823dc <mkyusds82a@hotmail.com>
2025-09-26 22:02:22 +0800

    Add files via upload

---
+++ jzz/js/common.js
@@ -0,0 +1,13 @@
+// $(function(){
+//     $('img').each(function() {
+//         var href = $(this).attr('data-href');
+//         if (href && href.trim() !== '') {
+//             // 设置鼠标为点击样式
+//             $(this).css('cursor', 'pointer');
+//             // 添加点击事件
+//             $(this).on('click', function() {
+//                 window.open(href, '_blank'); // 在新标签页打开链接
+//             });
+//         }
+//     });
+// })
